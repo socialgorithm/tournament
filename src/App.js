8b4ec20c50c9102f9a5c10dbe82afb6c4725caf6
Alejandro U. Alvarez <aluralma@gmail.com>
2017-03-02 12:49:33 +0000

    setup semantic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Menu, Container, Input } from 'semantic-ui-react';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Menu inverted>
+          <Menu.Item header
+            name='home'
+          >
+            Ultimate TTT Battle
+          </Menu.Item>
+
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Input icon='lightning' placeholder='http://localhost:3143' />
+            </Menu.Item>
+          </Menu.Menu>
+        </Menu>
+        <Container>
+          Things...
+        </Container>
       </div>
     );
   }
