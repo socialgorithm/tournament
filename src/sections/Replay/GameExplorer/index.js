0445025c0e8748c5b69303577cae2d98231387ce
Alejandro U. Alvarez <aluralma@gmail.com>
2017-03-15 18:47:31 +0000

    add moves table

--- src/sections/Replay/GameExplorer/index.js
+++ src/sections/Replay/GameExplorer/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header, Grid, List, Label } from 'semantic-ui-react';
+import { Header, Grid, List, Label, Table, Divider } from 'semantic-ui-react';
 import UTTT from 'ultimate-ttt';
 
 import UTTTGame from '../../../components/UTTTGame';
@@ -29,9 +29,10 @@ class GameExplorer extends React.Component {
     let curGame;
     lines.forEach((line) => {
       if (line === 'init') {
-        curGame = games.push(
-          new UTTT()
-        ) - 1;
+        curGame = games.push({
+          uttt: new UTTT(),
+          moves: [],
+        }) - 1;
         return;
       }
       if (line.indexOf(';') < 0) {
@@ -48,10 +49,15 @@ class GameExplorer extends React.Component {
         }
         const board = turn[0].split(',');
         const move = turn[1].split(',');
+        game.moves.push({
+          board,
+          move,
+          player: (opponent) ? 2 : 1,
+        });
         if (opponent) {
-          game.move(board, 2, move);
+          game.uttt.move(board, 2, move);
         } else {
-          game.move(board, 1, move);
+          game.uttt.move(board, 1, move);
         }
       } catch(e) {
         console.error('Error', line, curGame, games);
@@ -87,15 +93,45 @@ class GameExplorer extends React.Component {
             { this.state.games.map((game, $index) => (
               <List.Item key={ $index } active={ $index === this.state.activeGame } onClick={ () => { this.setState({ activeGame: $index }); } }>
                 <List.Content>
-                  { this.printWinner(game.winner) } { game.moves } moves
+                  { this.printWinner(game.uttt.winner) } { game.uttt.moves } moves
                 </List.Content>
               </List.Item>
             )) }
           </List>
         </Grid.Column>
         <Grid.Column width={ 12 }>
-          <Header>Game</Header>
-          <UTTTGame style={ { fontSize: '0.7em' } } game={ this.state.games[this.state.activeGame] } />
+          <Grid columns={ 2 } stackable>
+            <Grid.Column>
+              <Header>Game</Header>
+              <UTTTGame style={ { fontSize: '0.7em' } } game={ this.state.games[this.state.activeGame].uttt } />
+              <Divider />
+              <p><Label empty circular color='blue' horizontal /> You</p>
+              <p><Label empty circular color='red' horizontal /> Opponent</p>
+            </Grid.Column>
+            <Grid.Column>
+              <Header>Moves</Header>
+              <Table selectable inverted striped>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>#</Table.HeaderCell>
+                    <Table.HeaderCell>Board</Table.HeaderCell>
+                    <Table.HeaderCell>Move</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                  { this.state.games[this.state.activeGame].moves.map((data, $index) => (
+                    <Table.Row key={ $index }>
+                      <Table.Cell>
+                        <Label circular color={ (data.player === 1)? 'blue' : 'red' }>{ $index + 1 }</Label>
+                      </Table.Cell>
+                      <Table.Cell>{ data.board.join(', ') }</Table.Cell>
+                      <Table.Cell>{ data.move.join(', ') }</Table.Cell>
+                    </Table.Row>
+                  )) }
+                </Table.Body>
+              </Table>
+            </Grid.Column>
+          </Grid>
         </Grid.Column>
       </Grid>
     );
