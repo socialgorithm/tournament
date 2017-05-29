a064e27fcf628f850ba2ae4711d09b5342d14898
Eugene <eugene.07@list.ru>
2017-05-29 15:05:01 +0100

    Adds example tournament layout

--- src/containers/TournamentContainer/reducer.js
+++ src/containers/TournamentContainer/reducer.js
@@ -9,40 +9,68 @@ const gameStatuses = {
   FINISHED: 2
 };
 
-const initialGame = {
-  playerA: "playerA",
-  playerB: "playerB",
+const round1Game1 = {
+  playerA: "player1",
+  playerB: "player2",
+  winner: "player2",
+  result: gameStatuses.FINISHED
+};
+
+const round1Game2 = {
+  playerA: "player3",
+  playerB: "player4",
+  winner: "player3",
+  result: gameStatuses.FINISHED
+};
+
+const round1Game3 = {
+  playerA: "player5",
+  playerB: "player6",
+  winner: "player5",
+  result: gameStatuses.FINISHED
+};
+const round1Game4 = {
+  playerA: "player7",
+  playerB: "player8",
+  winner: "player7",
+  result: gameStatuses.FINISHED
+};
+
+const round2Game1 = {
+  playerA: "player2",
+  playerB: "player3",
+  winner: "player2",
+  result: gameStatuses.FINISHED
+};
+
+const round2Game2 = {
+  playerA: "player5",
+  playerB: "player7",
+  winner: "player5",
+  result: gameStatuses.FINISHED
+};
+
+const round3Game1 = {
+  playerA: "player2",
+  playerB: "player5",
   winner: null,
   result: gameStatuses.IN_PROGRESS
 };
 
-const initialGame1 = {
-  playerA: "playerA",
-  playerB: "playerB",
-  winner: "playerB",
-  result: gameStatuses.FINISHED
+const round1 = {
+  games: [round1Game1, round1Game2, round1Game3, round1Game4]
 };
 
-const initialGame2 = {
-  playerA: "playerC",
-  playerB: "playerB",
-  winner: "playerC",
-  result: gameStatuses.FINISHED
+const round2 = {
+  games: [round2Game1, round2Game2]
 };
 
-const isRoundFinished = (round) => round.games.every(game => game.result === gameStatuses.FINISHED);
-
-
-const initialRound = {
-  games: [initialGame, initialGame1]
-};
-
-const initialRound1 = {
-  games: [initialGame2]
+const round3 = {
+  games: [round3Game1]
 };
 
 const tournament = {
-  rounds: [initialRound, initialRound1]
+  rounds: [round1, round2, round3]
 };
 
 export const initialState = Map(tournament);
--- src/sections/Tournaments/index.js
+++ src/sections/Tournaments/index.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import { List } from 'semantic-ui-react';
+import './index.css';
 
+
+const gameLayout = (game, index) => (
+  <div key={index}>
+    <li className="spacer">&nbsp;</li>
+    <li className={"game game-top " + (game.playerA === game.winner ? "winner" : "")}>{game.playerA}</li>
+    <li className="game game-spacer">&nbsp;</li>
+    <li className={"game game-bottom " + (game.playerB === game.winner ? "winner" : "")}>{game.playerB}</li>
+    <li className="spacer">&nbsp;</li>
+  </div>
+);
 class Tournaments extends React.Component {
   render() {
-    debugger;
     return (
-        <div>
+        <main id="tournament">
           {
             this.props.rounds.map((round, $index) => (
               <div>
-                {
-                  round.games.map((game, $index) => (
-                      <div>
-                        { game.result }
-                      </div>
-                  ))
-                }
+                <ul className={ "round " + "round-" + ($index + 1)}>
+                  {
+                    round.games.map((game, index) => (
+                      gameLayout(game, index)
+                    ))
+                  }
+                </ul>
               </div>
             ))
           }
-        </div>
+        </main>
     );
   }
 }
