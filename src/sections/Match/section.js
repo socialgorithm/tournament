82033f0a17ea0a5482bb82a810a26443fbaa8a90
Alejandro U. Alvarez <aluralma@gmail.com>
2018-07-06 11:14:55 +0100

    fix disconnected socket detection

--- src/sections/Match/section.js
+++ src/sections/Match/section.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Icon, Message } from 'semantic-ui-react';
+import { Icon, Message, Container } from 'semantic-ui-react';
 
 import CreateMatch from '../../components/CreateMatch';
 import JoinMatch from '../../components/JoinMatch';
 
 export default (props) => {
-    if (!props.socket || !props.socket.socket) {
+    if (!props.socket || !props.socket.socket || !props.socket.socket.connected) {
         return (
-            <div>
+            <Container textAlign='center'>
                 <h1><Icon name='game' /><br /> Match</h1>
-                <Message>
-                    Please connect to the server first.
+                <Message compact warning>
+                    Please connect to the server first (top right button in the header)
                 </Message>
-            </div>
+            </Container>
         );
     }
 
