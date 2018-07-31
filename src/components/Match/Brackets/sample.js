1c43b3614c0c70576636bb15ed2ab2380e57bea2
Alejandro U. Alvarez <aluralma@gmail.com>
2018-07-31 18:22:09 +0100

    add sample data from the server

---
+++ src/components/Match/Brackets/sample.js
@@ -0,0 +1,160 @@
+export const SAMPLE_STATS = {
+    "options": {
+      "maxGames": 100,
+      "timeout": 100
+    },
+    "started": true,
+    "finished": false,
+    "matches": [
+      {
+        "stats": {
+          "games": 0,
+          "ties": 0,
+          "wins": [
+            0,
+            0
+          ],
+          "times": [],
+          "timeouts": [
+            0,
+            0
+          ],
+          "state": "finished"
+        },
+        "players": [
+          {
+            "token": "P1"
+          },
+          {
+            "token": "P2"
+          }
+        ]
+      },
+      {
+        "stats": {
+          "games": 0,
+          "ties": 0,
+          "wins": [
+            0,
+            0
+          ],
+          "times": [],
+          "timeouts": [
+            0,
+            0
+          ],
+          "state": "finished"
+        },
+        "players": [
+          {
+            "token": "P3"
+          },
+          {
+            "token": "P4"
+          }
+        ]
+      },
+      {
+        "stats": {
+          "games": 0,
+          "ties": 0,
+          "wins": [
+            0,
+            0
+          ],
+          "times": [],
+          "timeouts": [
+            0,
+            0
+          ],
+          "state": "finished"
+        },
+        "players": [
+          {
+            "token": "P1"
+          },
+          {
+            "token": "P4"
+          }
+        ]
+      },
+      {
+        "stats": {
+          "games": 0,
+          "ties": 0,
+          "wins": [
+            0,
+            0
+          ],
+          "times": [],
+          "timeouts": [
+            0,
+            0
+          ],
+          "state": "finished"
+        },
+        "players": [
+          {
+            "token": "P2"
+          },
+          {
+            "token": "P3"
+          }
+        ]
+      },
+      {
+        "stats": {
+          "games": 0,
+          "ties": 0,
+          "wins": [
+            0,
+            0
+          ],
+          "times": [],
+          "timeouts": [
+            0,
+            0
+          ],
+          "state": "finished"
+        },
+        "players": [
+          {
+            "token": "P1"
+          },
+          {
+            "token": "P2"
+          }
+        ]
+      },
+      {
+        "stats": {
+          "games": 0,
+          "ties": 0,
+          "wins": [
+            0,
+            0
+          ],
+          "times": [],
+          "timeouts": [
+            0,
+            0
+          ],
+          "state": "upcoming"
+        },
+        "players": [
+          {
+            "token": "P4"
+          },
+          {
+            "token": "P1"
+          }
+        ]
+      }
+    ],
+    "ranking": [
+      "P1",
+      "P4",
+      "P2",
+      "P3"
+    ]
+  };
