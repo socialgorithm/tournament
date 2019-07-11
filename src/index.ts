a78a692780808782099a9e2b995315588f43fec4
Bharat Reddy <me@bharatreddy.com>
2019-07-11 22:14:07 +0100

    Expose game socket messages

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { EVENTS } from "./Events";
 import { Game } from "./Game";
+import * as GameMessage from "./GameMessage";
+import { GAME_SOCKET_MESSAGE } from "./GameSocketMessage";
 import { Lobby } from "./Lobby";
 import { Match, MatchOptions } from "./Match";
 import * as MSG from "./Messages";
@@ -9,6 +11,8 @@ import { Tournament, TournamentOptions } from "./Tournament";
 export {
   EVENTS,
   Game,
+  GameMessage,
+  GAME_SOCKET_MESSAGE,
   Lobby,
   Match,
   MatchOptions,
