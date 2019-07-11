356b73706feb0ca8cadd1a1e272303aecbfaf43e
Bharat Reddy <me@bharatreddy.com>
2019-07-11 21:56:18 +0100

    Add Game Server models

---
+++ dist/GameSocketMessage.d.ts
@@ -0,0 +1,9 @@
+export declare const GAME_SOCKET_MESSAGE: {
+    GAME_ENDED: string;
+    GAME_INFO: string;
+    GAME_SERVER_HANDOFF: string;
+    GAME_STARTED: string;
+    GAME_UPDATED: string;
+    GAME__PLAYER: string;
+    START_GAME: string;
+};
---
+++ dist/GameSocketMessage.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.GAME_SOCKET_MESSAGE = {
+    GAME_ENDED: "GAME_ENDED",
+    GAME_INFO: "GAME_INFO",
+    GAME_SERVER_HANDOFF: "GAME_SERVER_HANDOFF",
+    GAME_STARTED: "GAME_STARTED",
+    GAME_UPDATED: "GAME_UPDATED",
+    GAME__PLAYER: "GAME__PLAYER",
+    START_GAME: "START_GAME"
+};
+//# sourceMappingURL=GameSocketMessage.js.map
