2c4d248992d0bfb1fce4cfe529eb116e8e8fb758
Bharat Reddy <me@bharatreddy.com>
2019-07-11 22:22:43 +0100

    Build on commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,8 +1,10 @@
 import { EVENTS } from "./Events";
 import { Game } from "./Game";
+import * as GameMessage from "./GameMessage";
+import { GAME_SOCKET_MESSAGE } from "./GameSocketMessage";
 import { Lobby } from "./Lobby";
 import { Match, MatchOptions } from "./Match";
 import * as MSG from "./Messages";
 import { Player } from "./Player";
 import { Tournament, TournamentOptions } from "./Tournament";
-export { EVENTS, Game, Lobby, Match, MatchOptions, MSG, Player, Tournament, TournamentOptions, };
+export { EVENTS, Game, GameMessage, GAME_SOCKET_MESSAGE, Lobby, Match, MatchOptions, MSG, Player, Tournament, TournamentOptions, };
--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,10 @@
 exports.__esModule = true;
 var Events_1 = require("./Events");
 exports.EVENTS = Events_1.EVENTS;
+var GameMessage = require("./GameMessage");
+exports.GameMessage = GameMessage;
+var GameSocketMessage_1 = require("./GameSocketMessage");
+exports.GAME_SOCKET_MESSAGE = GameSocketMessage_1.GAME_SOCKET_MESSAGE;
 var MSG = require("./Messages");
 exports.MSG = MSG;
 //# sourceMappingURL=index.js.map
