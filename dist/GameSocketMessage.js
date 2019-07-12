0581d03e39a2679b0fe7ec0683e47bbc4f45de66
Bharat Reddy <me@bharatreddy.com>
2019-07-12 13:56:17 +0100

    Use game created instead of game started

--- dist/GameSocketMessage.js
+++ dist/GameSocketMessage.js
@@ -1,12 +1,12 @@
 "use strict";
 exports.__esModule = true;
 exports.GAME_SOCKET_MESSAGE = {
+    CREATE_GAME: "CREATE_GAME",
+    GAME_CREATED: "GAME_CREATED",
     GAME_ENDED: "GAME_ENDED",
     GAME_INFO: "GAME_INFO",
     GAME_SERVER_HANDOFF: "GAME_SERVER_HANDOFF",
-    GAME_STARTED: "GAME_STARTED",
     GAME_UPDATED: "GAME_UPDATED",
-    GAME__PLAYER: "GAME__PLAYER",
-    CREATE_GAME: "CREATE_GAME"
+    GAME__PLAYER: "GAME__PLAYER"
 };
 //# sourceMappingURL=GameSocketMessage.js.map
