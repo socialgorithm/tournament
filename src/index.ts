cf02b5c6feb29915300791286d418f1cfe3b8dbb
unknown <joffat@users.noreply.github.com>
2020-07-24 14:23:08 +0100

    Adding Args Handler For Command Line

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
-import { DEFAULT_OPTIONS } from "./cli/options";
+import getOptions, {DEFAULT_OPTIONS} from "./cli/options";
 import TournamentServer from "./TournamentServer";
 
-const server = new TournamentServer(DEFAULT_OPTIONS);
+const server = new TournamentServer({
+    ...DEFAULT_OPTIONS,
+    ...getOptions()
+});
