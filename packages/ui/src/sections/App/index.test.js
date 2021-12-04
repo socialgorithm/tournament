9d07993866cb0e2a081696368a74225901072efa
Bharat Reddy <me@bharatreddy.com>
2021-12-04 15:31:06 +0000

    Fix UI test

--- packages/ui/src/sections/App/index.test.js
+++ packages/ui/src/sections/App/index.test.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../App';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router/immutable';
+import Immutable from 'immutable';
+
+import history from '../../utils/history';
+import configureStore from '../../store';
+
+const initialState = Immutable.Map();
+const store = configureStore(initialState, history);
+const MOUNT_NODE = document.getElementById('root');
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={ store }>
+      <ConnectedRouter history={history}>
+          <App />
+      </ConnectedRouter>
+    </Provider>, div);
 });
