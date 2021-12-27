7f68f3f1abb95da2a356cecf0d8f797c651609d6
Bharat Reddy <me@bharatreddy.com>
2021-12-27 20:52:13 +0000

    Explicit ignore for eslint require

--- packages/server/src/bin/index.js
+++ packages/server/src/bin/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 const server = require('../cli/index')["default"];
 const options = require('../cli/options')["default"]();
